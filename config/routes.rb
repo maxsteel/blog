bf31120ea1a50453a95907fad84748de45498d68
Ravi Singh <maxsteel.ravi26@gmail.com>
2012-12-03 10:48:33 +0530

    post generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+  resources :posts
+
+  get "home/index"
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +54,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
